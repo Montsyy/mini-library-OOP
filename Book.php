3f8165e33bbc11a3b7793ceffae99c2e39bd735c
Montsyy <dhafifarezi6@gmail.com>
2026-09-04 08:24:35 +0700

    Implement Book class with properties and methods for managing book details

---
+++ Book.php
@@ -0,0 +1,41 @@
+<?php
+
+class Book {
+    private string $title;
+    private string $author;
+    private int $year;
+    private bool $isAvailable = true;
+
+
+    public function __construct(string $title, string $author, int $year) {
+        $this->title = $title;
+        $this->author = $author;
+        $this->year = $year;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getAuthor() {
+        return $this->author;
+    }
+
+    public function getYear() {
+        return $this->year;
+    }
+
+    public function pinjambuku(): bool {
+        if (!$this->isAvailable) {
+            return false;
+        }
+        $this->isAvailable = false;
+        return true;
+    }
+
+    public function getinfobuku(): string {
+        $status = $this->isAvailable ? 'Tersedia' : 'Tidak Tersedia';
+        return "Title: {$this->title}, Author: {$this->author}, Year: {$this->year}, Status: {$status}";
+
+    }
+}
